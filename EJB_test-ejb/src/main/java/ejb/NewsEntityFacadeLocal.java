4f46582daacb179c4d6a730519faac952f1b6cf9
Pawel <Pawel@Pawel-Komputer>
2014-10-12 13:17:12 +0200

    Adding project

---
+++ EJB_test-ejb/src/main/java/ejb/NewsEntityFacadeLocal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejb;
+
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Pawel
+ */
+@Local
+public interface NewsEntityFacadeLocal {
+
+    void create(NewsEntity newsEntity);
+
+    void edit(NewsEntity newsEntity);
+
+    void remove(NewsEntity newsEntity);
+
+    NewsEntity find(Object id);
+
+    List<NewsEntity> findAll();
+
+    List<NewsEntity> findRange(int[] range);
+
+    int count();
+    
+}
